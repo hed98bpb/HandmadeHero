118eb35ffa31ea4eaf9f274f8d7e012841ca6eae
hed98bpb <loleparty@yahoo.dk>
2017-02-21 00:38:00 +0100

    day 1

--- code/build.bat
+++ code/build.bat
@@ -1,2 +1,6 @@
 @echo off
 
+mkdir ..\build
+pushd ..\build
+cl -Zi ..\code\win32_handmade.cpp user32.lib
+popd
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -3,8 +3,10 @@
 int CALLBACK
 WinMain(HINSTANCE hInstance,
 		     HINSTANCE hPrevInstance,
-		     LPSTR     lpCmdLine,
-		     int       nCmdShow)
+		     LPSTR lpCmdLine,
+		     int nCmdShow)
 {
+	MessageBox(0, "This is Handmade Hero", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+
   return(0);
 }
